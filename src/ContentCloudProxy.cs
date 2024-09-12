a89646ef6852b83dc45f82957ead4a3b070678bf
Pranav Guruprasad Rao <pranavrao2500@gmail.com>
2024-09-12 23:38:50 +0530

    Update ContentCloudProxy.cs

--- src/ContentCloudProxy.cs
+++ src/ContentCloudProxy.cs
@@ -32,7 +32,7 @@ public class ContentCloudProxy {
 
     string Post(string folder, object data)
     {
-        return cloudService.Put(userAuth, folder, data)
+        return cloudService.Post(userAuth, folder, data)
     }
 
     bool Put(string folder, string oldDataUri, object data)
