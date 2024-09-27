5ceae0856f2f2239abd4f2a7dce51ad1e203a317
unknown <arnavkaducr7@gmail.com>
2024-09-27 12:15:19 +0530

    added example code

---
+++ TestBlobStorage/TestBlobStorage/TestBlob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Identity;
+
+namespace TestBlobStorage
+{
+    public class TestBlob
+    {
+    }
+}
